84065801ec5d6dbc97f9cf0277e43cf23ca7d0ea
Haikal Fikri <haikaljr1245@gmail.com>
2024-05-30 16:24:06 +0700

    make event model

--- ComeNow/events/models.py
+++ ComeNow/events/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class EventModel(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+    location = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="images/")
+    contact = models.CharField(max_length=12)
+    event_date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
+    more_information = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+    @property
+    def imageUrl(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
